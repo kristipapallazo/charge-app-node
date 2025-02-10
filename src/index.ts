37a4dbe1f9c656eaab918ace2ebdbd700b660e79
Kristi Papallazo <papallazokristi@gmail.com>
2025-02-10 17:25:39 +0100

    .

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import router from "./routes/routes.ts";
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
